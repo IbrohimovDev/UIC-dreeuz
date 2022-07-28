5e8f6b84c1d4adb90123979d5d4c471512e46ce0
abdiroziqov <ilhomabdiroziqov@gmail.com>
2022-07-28 12:24:23 +0500

    slider

---
+++ script.js
@@ -0,0 +1,28 @@
+const root = document.getElementById('sliders')
+
+for (let i = 0; i < 3; i++) {
+    const carusel = document.createElement('div')
+    carusel.classList.add('carusel')
+    root.append(carusel)
+
+    const track = document.createElement('div')
+    track.classList.add('slide-track')
+    track.classList.add('relative')
+    track.style.display = 'flex'
+    carusel.append(track)
+
+    for (let j = 1; j <= 15; j++) {
+
+        const slide = document.createElement('div')
+        slide.classList.add('slide')
+        track.append(slide)
+
+        const image = document.createElement("img");
+        image.setAttribute("src", "images/" + j + ".jpg");
+        image.src="./assets/images/slider/sliderperson.png"
+        slide.append(image)
+    }
+
+}
+
+
---
+++ swiper.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle'
+
+
+const swiper = new Swiper(".mySwiper", {
+    modules: [Navigation, Pagination],
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    Pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    Navigation: {
+      nextEl: ".next",
+      prevEl: ".prev",
+    },
+  });
+
+
